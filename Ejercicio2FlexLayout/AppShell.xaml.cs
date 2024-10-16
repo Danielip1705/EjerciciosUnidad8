aef1edf6ff3bb7a8d7573ad20a23d4b3d798c753
diglesias <diglesias@107-05>
2024-10-16 14:36:59 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio2FlexLayout/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio2FlexLayout
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ejercicios/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicios
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
