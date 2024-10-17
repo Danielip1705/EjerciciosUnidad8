ca6d4abdbfcc0ebdc070fbf634eb456b713e9001
diglesias <diglesias@107-05>
2024-10-17 10:43:08 +0200

    Ejercicio 3 realizado

---
+++ Ejercicio3AbsoluteLayout/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio3AbsoluteLayout
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ejercicio3AbsoluteLayout/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Ejercicio3AbsoluteLayout
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    
+    }
+
+}
